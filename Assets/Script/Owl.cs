25386bd6668b0af250b78675deab75f31124ec2e
Vincent Wisnata <wisnatavincent@gmail.com>
2021-05-03 16:08:50 +0700

    first

---
+++ Assets/Script/Owl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    public float radius;
+    public float power;
+    public float torque;
+    public bool _hasBoost = false;
+    public GameObject Explotion_Object;
+    public override void OnTap()
+    {
+        Explotion();
+    }
+
+    void Explotion()
+    {
+        Vector3 explotionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explotionPos, radius);
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+            if (rb != null && !_hasBoost)
+            {
+                rb.AddRelativeForce(explotionPos * power, ForceMode2D.Impulse);
+                rb.AddTorque(torque, ForceMode2D.Impulse);
+                _hasBoost = true;
+                Instantiate(Explotion_Object, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
